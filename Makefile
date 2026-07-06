a47cdb3e5f69a29a393291ba409c09b3ae79a46c
bis29abena <bismarkoseiabena@gmail.com>
2026-07-06 18:21:49 +0000

    added operator token

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-OBJECTS= ./build/compiler.o ./build/cprocess.o ./build/lexer.o ./build/lex_process.o ./build/helpers/buffer.o ./build/helpers/vector.o
+OBJECTS= ./build/compiler.o ./build/cprocess.o ./build/lexer.o ./build/token.o ./build/lex_process.o ./build/helpers/buffer.o ./build/helpers/vector.o
 INCLUDES= -I./
 
 all: ${OBJECTS}
@@ -13,6 +13,9 @@ all: ${OBJECTS}
 ./build/lexer.o: ./lexer.c
 	gcc ./lexer.c ${INCLUDES} -o ./build/lexer.o -g -c
 
+./build/token.o: ./token.c
+	gcc ./token.c ${INCLUDES} -o ./build/token.o -g -c
+
 ./build/lex_process.o: ./lex_process.c
 	gcc ./lex_process.c ${INCLUDES} -o ./build/lex_process.o -g -c
 
@@ -21,6 +24,8 @@ all: ${OBJECTS}
 
 ./build/helpers/vector.o: ./helpers/vector.c
 	gcc ./helpers/vector.c ${INCLUDES} -o ./build/helpers/vector.o -g -c
+
+
 	
 clean:
 	rm ./main
--- compiler.h
+++ compiler.h
@@ -2,39 +2,62 @@
 #define ORANGE_COMPILER_H
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
+#define S_EQ(str, str2) \
+    (str && str2 && (strcmp(str, str2) == 0))
 
 struct pos
 {
     int line;
     int col;
-    const char* filename;
+    const char *filename;
 };
 
 #define NUMERIC_CASE \
-    case '0':      \
-    case '1':      \
-    case '2':      \
-    case '3':      \
-    case '4':      \
-    case '5':      \
-    case '6':      \
-    case '7':      \
-    case '8':      \
-    case '9'      
+    case '0':        \
+    case '1':        \
+    case '2':        \
+    case '3':        \
+    case '4':        \
+    case '5':        \
+    case '6':        \
+    case '7':        \
+    case '8':        \
+    case '9'
 
-enum {
+#define OPERATOR_CASE_EXCLUDING_DIVISION \
+    case '+':                            \
+    case '-':                            \
+    case '*':                            \
+    case '%':                            \
+    case '=':                            \
+    case '<':                            \
+    case '>':                            \
+    case '!':                            \
+    case '&':                            \
+    case '|':                            \
+    case '^':                            \
+    case '~':                            \
+    case '(':                            \
+    case '[':                            \
+    case ',':                            \
+    case '.':                            \
+    case '?'
+
+enum
+{
     TOKEN_TYPE_IDENTIFIER,
     TOKEN_TYPE_KEYWORD,
     TOKEN_TYPE_OPERATOR,
     TOKEN_TYPE_SYMBOL,
     TOKEN_TYPE_STRING,
-    TPOKEN_TYPE_NUMBER,
+    TOKEN_TYPE_NUMBER,
     TOKEN_TYPE_COMMENT,
     TOKEN_TYPE_NEWLINE,
 };
 
-enum 
+enum
 {
     LEXICAL_ANALYSIS_ALL_OK,
     LEXICAL_ANALYSIS_INPUT_ERROR
@@ -46,22 +69,22 @@ struct token
     int flags;
     struct pos pos;
 
-    union 
+    union
     {
         char cval;
-        const char* sval;
+        const char *sval;
         unsigned int inum;
         unsigned long lnum;
         unsigned long long llnum;
-        void* any;
+        void *any;
     };
-    
+
     // True if the is a whitespace between the token and the next token.
     // i.e * a for operator token * would mean whitepsace would be set for token "a"
 
     bool whitespace;
 
-    const char* between_brackets;
+    const char *between_brackets;
 };
 
 struct lex_process;
@@ -79,17 +102,16 @@ struct lex_process_functions
 struct lex_process
 {
     struct pos pos;
-    struct vector* token_vec;
-    struct compile_process* compiler;
-
+    struct vector *token_vec;
+    struct compile_process *compiler;
 
     int current_expression_count;
-    struct buffer* parenthesis_buffer;
-    struct lex_process_functions* function;
+    struct buffer *parenthesis_buffer;
+    struct lex_process_functions *function;
 
-    //This will be private data that the lexer does not understand
-    //but the person using the lexer does understand. This is useful for passing data to the lexer that it does not need to understand
-    void* private;
+    // This will be private data that the lexer does not understand
+    // but the person using the lexer does understand. This is useful for passing data to the lexer that it does not need to understand
+    void *private;
 };
 
 enum
@@ -100,35 +122,37 @@ enum
 
 struct compile_process
 {
-    //the flags in regards to how this file should be compiled
+    // the flags in regards to how this file should be compiled
     int flags;
 
     struct pos pos;
     struct compile_process_input_file
     {
-        FILE* fp;
-        const char* abs_path;
+        FILE *fp;
+        const char *abs_path;
     } cfile;
 
-    FILE* ofile;
+    FILE *ofile;
 };
 
-void compiler_error(struct compile_process* process, const char* message, ...);
+void compiler_error(struct compile_process *process, const char *message, ...);
 
-int compile_file(const char *filename, const char* out_filename, int flags);
+int compile_file(const char *filename, const char *out_filename, int flags);
 struct compile_process *compile_process_create(const char *filename, const char *out_filename, int flags);
 char compile_process_next_char(struct lex_process *lex_process);
-char compile_process_peek_char(struct lex_process* lex_process);
-void compile_process_push_char(struct lex_process* lex_process, char c);
+char compile_process_peek_char(struct lex_process *lex_process);
+void compile_process_push_char(struct lex_process *lex_process, char c);
 
-struct lex_process* lex_process_create(struct compile_process* compiler, struct lex_process_functions* functions, void* private);
-void lex_process_free(struct lex_process* process);
-void* lex_process_private(struct lex_process* process);
-struct vector* lex_process_token(struct lex_process* process);
+struct lex_process *lex_process_create(struct compile_process *compiler, struct lex_process_functions *functions, void *private);
+void lex_process_free(struct lex_process *process);
+void *lex_process_private(struct lex_process *process);
+struct vector *lex_process_token(struct lex_process *process);
 
-void compiler_error(struct compile_process* process, const char* message, ...);
-void compiler_warning(struct compile_process* process, const char* message, ...);
+void compiler_error(struct compile_process *process, const char *message, ...);
+void compiler_warning(struct compile_process *process, const char *message, ...);
 
-int lex(struct lex_process* process);
+int lex(struct lex_process *process);
+
+bool token_is_keyword(struct token* token, const char* value);
 
 #endif // ORANGE_COMPILER_H

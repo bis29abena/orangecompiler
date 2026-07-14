a53ce6b2021c5d66310facb1d525faf34d6e1a60
bis29abena <bismarkoseiabena@gmail.com>
2026-07-14 14:08:17 +0000

    implemented our node file

---
+++ node.c
@@ -0,0 +1,42 @@
+#include "compiler.h"
+#include "helpers/vector.h"
+#include <assert.h>
+
+struct vector* node_vec = NULL;
+struct vector* node_vector_root = NULL;
+
+void node_set_vector(struct vector* vec, struct vector* root)
+{
+    node_vec = vec;
+    node_vector_root = root;
+}
+
+void node_push(struct node* node)
+{
+    vector_push(node_vec, &node);
+}
+
+struct node* node_peek_or_null()
+{
+    return vector_back_ptr_or_null(node_vec);
+}
+
+struct node* node_peek()
+{
+    return *(struct node**)(vector_back(node_vec));
+}
+
+struct node* node_pop()
+{
+    struct node* last_node = vector_back_ptr(node_vec);
+    struct node* last_node_root = vector_empty(node_vec) ? NULL : vector_back_ptr(node_vector_root);
+    
+
+    vector_pop(node_vec);
+    if(last_node_root == last_node)
+    {
+        vector_pop(node_vector_root);
+    }
+
+    return last_node;
+}
